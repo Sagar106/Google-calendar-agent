fd42c98fe4bcc0a380acc842cb1fd1ab138514bd
Sagar Upadhyay <sagarupadhyay106@gmail.com>
2026-07-18 23:13:42 +0530

    Implementing get event tool

--- plan.md
+++ plan.md
@@ -3,8 +3,10 @@
 1. Project setup - done
 2. LLM Setup - done
 3. Setup the tools - Create event tool, Get event tool - done
-4. Build the graph
-5. Implement tools - Connect to google calendar
+4. Build the graph - done
+5. Implement tools :
+    get event - done
+    create event -
 6. Test the agent
 7. Terminal or Chat UI
 8. Adding memory
--- src/tools.ts
+++ src/tools.ts
@@ -1,5 +1,72 @@
 import { tool } from "@langchain/core/tools";
+import { google } from "googleapis";
 import z from "zod";
+import dotenv from "dotenv";
+import tokens from "./tokens.json" with { type: "json" };
+
+dotenv.config();
+
+const oauth2Client = new google.auth.OAuth2(
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+    process.env.GOOGLE_REDIRECT_URL
+);
+
+oauth2Client.setCredentials(tokens)
+
+const calendar = google.calendar({ version: 'v3', auth: oauth2Client });
+
+type Params = {
+    q: string;
+    timeMin: string;
+    timeMax: string;
+}
+
+export const getEvents = tool(
+    async (params) => {
+        const { q, timeMin, timeMax } = params as Params;
+
+        try {
+            const response = await calendar.events.list({
+                calendarId: "primary",
+                q: q,
+                timeMin,
+                timeMax,
+            })
+
+            console.log("Events: ", response.data.items)
+
+            const result = response.data.items?.map((event) => {
+                return {
+                    id: event.id,
+                    summary: event.summary,
+                    status: event.status,
+                    orgnizer: event.organizer?.email,
+                    start: event.start,
+                    end: event.end,
+                    attendees: event.attendees?.map((attendee) => attendee.email),
+                    meetingLink: event.hangoutLink,
+                    eventType: event.eventType,
+                }
+            })
+
+            return JSON.stringify(result)
+        } catch (error) {
+            console.log("Error fetching events: ", error);
+        }
+
+        return "Failed to connect to the Calendar."
+    },
+    {
+        name: "get-calendar-events",
+        description: "Call to get the calendar events.",
+        schema: z.object({
+            q: z.string().describe("The query to be used to get google calendar events. It can be one of these values: summary, description, location, attendees email, organiser's name, organiser's email"),
+            timeMin: z.string().describe("The from datetime in UTC format for the event."),
+            timeMax: z.string().describe("The to datetime in UTC format for the event."),
+        }),
+    },
+);
 
 export const createEvent = tool(
     async () => {
@@ -11,22 +78,6 @@ export const createEvent = tool(
         description: "Call to create a new calendar event.",
         schema: z.object({
             query: z.string().describe("The query to be used to create google calendar event."),
-        }),
-    },
-);
-
-export const getEvents = tool(
-    async () => {
-        // Google calendar logic goes here
-        return JSON.stringify([
-            { title: "Meeting with Sujoy", time: "2 PM", location: "Google Meet" },
-        ]);
-    },
-    {
-        name: "get-calendar-events",
-        description: "Call to get the calendar events.",
-        schema: z.object({
-            query: z.string().describe("The query to be used to get google calendar events."),
         }),
     },
 );
